cdcfc040b51b7912f37cbff2791b5fb72e7a182e
Adarsh Muthukumaran <adarshh2313@gmail.com>
2022-02-17 22:33:33 +0530

    angle calc between keypoints done

--- pose_detection/detect.py
+++ pose_detection/detect.py
@@ -1,18 +1,31 @@
-from cv2 import KeyPoint
+from cmath import pi
 from tf_pose import common
 import cv2
-import numpy as np
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 import matplotlib.pyplot as plt
+import numpy as np
+
+def anglecalc(keypoint_list,n1,n2,n3):
+    
+    _,x1,y1 = keypoint_list[n1]
+    _,x2,y2 = keypoint_list[n2]
+    _,x3,y3 = keypoint_list[n3]
+
+    p1 = np.array([x1,y1]) - np.array([x2,y2])
+    p2 = np.array([x3,y3]) - np.array([x2,y2])
+    cosine_angle = np.dot(p1, p2) / (np.linalg.norm(p1) * np.linalg.norm(p2))
+    angle = np.arccos(cosine_angle)
+
+    return np.degrees(angle)
 
 if __name__ == '__main__':
 
-    image = 'demo/demo_99.png'       # image path
-    w, h = model_wh('432x368')       # image resolution
+    image = 'demo/demo_4.jpeg'       # image path
+    image = common.read_imgfile(image, None, None)
+    w, h = model_wh('432x368')       # image resolution - fix at 432x368
 
     e = TfPoseEstimator(get_graph_path('cmu'), target_size=(w, h))
-    image = common.read_imgfile(image, None, None)
     humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=4.0)
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
     image = cv2.resize(image,dsize=(432,368))
@@ -29,14 +42,14 @@ if __name__ == '__main__':
         people['Person'+str(i+1)] = coordinates
 
     people = dict(sorted(people.items(), key=lambda item: item[1][0][1]))
-
-    people_new = {}
-
+    
+    people_coordinates = {}
     for k in range(0,len(people)):
-        temp = people[str(list(people.items())[k][0])]
-        people_new['Person'+str(k+1)] = temp
+        people_coordinates['Person'+str(k+1)] = people[str(list(people.items())[k][0])]
 
-    print(people_new)
 
+    print(people_coordinates)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    angle = anglecalc(people_coordinates['Person1'],1,8,9)
+    print(angle)
     plt.show()
